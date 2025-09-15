96ee31d89229a123fa6cb2793e475e634709f3be
Abdikhaliq Jama <Abdelkhalekjama@gmail.com>
2025-09-15 19:50:29 +0300

    Initial commit: replication project setup

---
+++ main.py
@@ -0,0 +1,12 @@
+print("hello world")
+#This is the firt time in my life I am writing a python code
+#I am very excited to learn python
+#I hope I will be able to write more complex code in the future
+#I am looking forward to building amazing projects with Python and doing all sorts of econometric analysis
+x = 4 
+print (x)
+y = 5
+print(y)
+z = x + y
+print(z)
+#This is a simple addition of two numbers
